4e544cec235e585aded2d0e59e60652fac18120d
Andrea <andreagirone2710@gmail.com>
2025-02-12 18:46:09 +0100

    Blanks sidepages

---
+++ script.js
@@ -0,0 +1,25 @@
+let regolamentoCard = document.querySelector('#regolamento');
+let attrezzaturaCard = document.querySelector('#attrezzatura');
+let modalitaDiGiocoCard = document.querySelector('#modalita-di-gioco');
+let colpiTecniciFondamentaliCard = document.querySelector('#colpi-tecnici-fondamentali');
+let storiaCard = document.querySelector('#storia');
+
+regolamentoCard = addEventListener('click', function() {
+	this.window.open("pages/regolamento.html", "_self");
+});
+
+regolamentoCard = addEventListener('click', function() {
+	this.window.open("pages/attrezzatura.html", "_self");
+});
+
+regolamentoCard = addEventListener('click', function() {
+	this.window.open("pages/modalita-di-gioco.html", "_self");
+});
+
+regolamentoCard = addEventListener('click', function() {
+	this.window.open("pages/colpi-tecnici-fondamentali.html", "_self");
+});
+
+regolamentoCard = addEventListener('click', function() {
+	this.window.open("pages/storia.html", "_self");
+});
